7b0276faa238150dfbe65c678f3ab7a582d46f18
lanling.zjw <lanling.zjw@alibaba-inc.com>
2021-05-14 11:05:49 +0800

    add rpc

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync/atomic"
+)
 import "rsc.io/quote"
 
 func main() {
+	a := int32(0)
+	atomic.AddInt32(&a, 1)
+	fmt.Println(a)
+	atomic.AddInt32(&a, 1)
+	fmt.Println(a)
 	fmt.Println("hello world!")
 	fmt.Println(quote.Go())
 }
